d2ac61109f25fca4ed595c1476440645f89f67fd
Thomas Thimothee <thom.thimothee@gmail.com>
2017-10-31 21:22:26 +0100

    sorting

--- seedClient/src/pages/App2.js
+++ seedClient/src/pages/App2.js
@@ -5,17 +5,17 @@ export default class App2 extends Component {
     
       constructor() {
         super();
-        const data ={places: [ {address: {city: 'Toulon', zip: "1111", street: "aa", location: "aa"}, rating: "3"},
-        {address: {city: 'Marseille', zip: "22222", street: "bb", location: "bb"}, rating: "2"},
-        {address: {city: 'Paris', zip: "3333", street: "cc", location: "cc"}, rating: "1"},
-        {address: {city: 'Londres', zip: "44444", street: "dd", location: "dd"}, rating: "4"}
+        this.data ={places: [ {rating: "5"},
+        { rating: "23"},
+        {rating: "4"},
+        {rating: "444"}
         ]}
+        console.log("constructor", this.data)
       }
 
       render() {
-        console.log("obj ",this.data)
-        console.log("obj.places ",this.data.places)
-          var sortedArray = pf.sortByRating(data.places)
+        console.log("this.data.places ",this.data.places)
+          var sortedArray = pf.sortByRating(this.data.places)
           console.log("new Array", sortedArray)
         return (
           <div>
