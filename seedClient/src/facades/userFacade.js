dd715a04d2ce7377e107a71ee272dc38e068ef01
lovro <cph-lb226@cphbusiness.dk>
2017-10-31 11:09:06 +0100

    added signup to seedClient

--- seedClient/src/facades/userFacade.js
+++ seedClient/src/facades/userFacade.js
@@ -1,4 +1,4 @@
-import fetchHelper, {errorChecker} from "./fetchHelpers"
+import fetchHelper, { errorChecker } from "./fetchHelpers"
 const URL = require("../../package.json").serverURL;
 
 class UserStore {
@@ -10,14 +10,14 @@ class UserStore {
   getData = (cb) => {
     this._errorMessage = "";
     this._messageFromServer = "";
-    let resFromFirstPromise=null;  //Pass on response the "second" promise so we can read errors from server
+    let resFromFirstPromise = null;  //Pass on response the "second" promise so we can read errors from server
     const options = fetchHelper.makeOptions("GET", true);
     fetch(URL + "api/demouser", options)
       .then((res) => {
         resFromFirstPromise = res;
         return res.json();
       }).then((data) => {
-        errorChecker(resFromFirstPromise,data);
+        errorChecker(resFromFirstPromise, data);
         if (cb) {
           cb(null, data.message)
         }
@@ -28,10 +28,34 @@ class UserStore {
         }
       })
   }
+
+
+  signUp = (props) => {
+    fetch(URL + 'api/user'), {
+      method: 'POST',
+      header: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+
+      },
+      body: JSON.stringify({
+        fName: props.fName,
+        lName: props.lName,
+        username: props.userName,
+        password: props.pass,
+        phone: props.phone,
+        email: props.email
+      })
+        .then((response) => response.json())
+        .then((responseJson) => {
+          return responseJson;
+        })
+    }
+  }
 }
 
-let userStore = new UserStore();
+    let userStore = new UserStore();
 
-//Only for debugging
-//window.userStore = userStore;
-export default userStore;
+    //Only for debugging
+    //window.userStore = userStore;
+    export default userStore;
