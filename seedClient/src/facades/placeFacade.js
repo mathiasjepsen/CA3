5c03ded3375900d6fb3adcc21c5a7c10df32a314
Thomas Thimothee <thom.thimothee@gmail.com>
2017-11-01 07:57:48 +0100

    sorting by city and zip

--- seedClient/src/facades/placeFacade.js
+++ seedClient/src/facades/placeFacade.js
@@ -3,9 +3,21 @@
 class placeFacade {
 
     sortByRating = (props) => {
-       var oldArray = props;
-       var sortedArray = oldArray.sort(compareRating);
+       let oldArray = props;
+       let sortedArray = oldArray.sort(compareRating);
        return sortedArray;
+    }
+
+    sortByCity = (props) =>{
+      let oldArray = props;
+      let sortedArray = oldArray.sort(compareCity);
+      return sortedArray;
+    }
+
+    sortByZip = (props) =>{
+      let oldArray = props;
+      let sortedArray = oldArray.sort(compareZip);
+      return sortedArray;
     }
 }
 
@@ -13,6 +25,22 @@ function compareRating(a,b) {
     if (parseInt(a.rating) > parseInt(b.rating))
       return -1;
     if (parseInt(a.rating) < parseInt(b.rating))
+      return 1;
+    return 0;
+  }
+
+  function compareCity(a,b) {
+    if (a.address.city < b.address.city)
+      return -1;
+    if (a.address.city > b.address.city)
+      return 1;
+    return 0;
+  }
+
+  function compareZip(a,b) {
+    if (a.address.zip < b.address.zip)
+      return -1;
+    if (a.address.zip > b.address.zip)
       return 1;
     return 0;
   }
--- seedClient/src/pages/App2.js
+++ seedClient/src/pages/App2.js
@@ -5,18 +5,22 @@ export default class App2 extends Component {
     
       constructor() {
         super();
-        this.data ={places: [ {rating: "5"},
-        { rating: "23"},
-        {rating: "4"},
-        {rating: "444"}
+        this.data ={places: [ { address:{city: "Toulon", zip: "a83140", street: "Avenue AA", location: "location"}, description:"this description" , rating: "5"},
+        {  address:{city: "London", zip: "234e53", street: "Avenue CC", location: "location"}, description:"this description" , rating: "23"},
+        { address:{city: "Barcelona", zip: "958ff35", street: "Avenue BB", location: "location"}, description:"this description" , rating: "4"},
+        { address:{city: "Copenhagen", zip: "8aar5140", street: "Boulevard AA", location: "location"}, description:"this description" , rating: "444"}
         ]}
         console.log("constructor", this.data)
       }
 
       render() {
         console.log("this.data.places ",this.data.places)
-          var sortedArray = pf.sortByRating(this.data.places)
-          console.log("new Array", sortedArray)
+          // var sortedbyRating = pf.sortByRating(this.data.places)
+          // console.log("sortedbyRating", sortedbyRating)
+          // var sortedbyCity = pf.sortByCity(this.data.places)
+          // console.log("sortedbyCity", sortedbyCity)
+          var sortedbyZip = pf.sortByZip(this.data.places)
+          console.log("sortedbyZip", sortedbyZip)
         return (
           <div>
             <h2>test</h2>
