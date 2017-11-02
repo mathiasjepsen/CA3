9d810010cf53af8ec410c856352f192996dde324
Thomas Thimothee <thom.thimothee@gmail.com>
2017-11-02 14:32:07 +0100

    .

--- seedServer/src/main/java/deploy/DeploymentConfiguration.java
+++ seedServer/src/main/java/deploy/DeploymentConfiguration.java
@@ -1,15 +1,11 @@
 package deploy;
 
-import entity.Address;
-import entity.Place;
 import entity.Role;
 import entity.User;
 import facades.UserFacade;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
 import java.util.logging.Level;
@@ -20,39 +16,38 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
-import security.IUser;
-import security.PasswordStorage;
 import security.Secret;
 
 @WebListener
 public class DeploymentConfiguration implements ServletContextListener {
 
-    public static String PU_NAME = "PU-Local";
+  public static String PU_NAME = "PU-Local";
 
-    @Override
-    @SuppressWarnings("empty-statement")
-    public void contextInitialized(ServletContextEvent sce) {
-        System.out.println("######################################################################################");
-        System.out.println("############################ In ContextIntialized ####################################");
-        System.out.println("######################################################################################");
+  @Override
+  @SuppressWarnings("empty-statement")
+  public void contextInitialized(ServletContextEvent sce) {
+    System.out.println("######################################################################################");
+    System.out.println("############################ In ContextIntialized ####################################");
+    System.out.println("######################################################################################");
 
-        //Handling init-params from the properties file (secrets that should not be pushed to GIT)
-        InputStream input = null;
-        Properties prop = new Properties();
-        try {
-            input = getClass().getClassLoader().getResourceAsStream("/config.properties");;
-            if (input == null) {
-                System.out.println("Could not load init-properties");
-                return;
-            }
-            prop.load(input);
-            Secret.SHARED_SECRET = prop.getProperty("tokenSecret").getBytes();
-            input.close();
+    //Handling init-params from the properties file (secrets that should not be pushed to GIT)
+    InputStream input = null;
+    Properties prop = new Properties();
+    try {
+      input = getClass().getClassLoader().getResourceAsStream("/config.properties");;
+      if (input == null) {
+        System.out.println("Could not load init-properties");
+        return;
+      }
+      prop.load(input);
+      Secret.SHARED_SECRET = prop.getProperty("tokenSecret").getBytes();
+      input.close();
 
-        } catch (IOException ex) {
-            Logger.getLogger(DeploymentConfiguration.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        ServletContext context = sce.getServletContext();
+    } catch (IOException ex) {
+      Logger.getLogger(DeploymentConfiguration.class.getName()).log(Level.SEVERE, null, ex);
+    }
+    ServletContext context = sce.getServletContext();
+ 
 
 //  UserFacade uf = new UserFacade(Persistence.createEntityManagerFactory("pu_development"));
 //
@@ -112,7 +107,7 @@ public class DeploymentConfiguration implements ServletContextListener {
 //            em.close();
 //        }
         
-    }
+  }
 
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
--- seedServer/src/main/java/rest/ApplicationConfig.java
+++ seedServer/src/main/java/rest/ApplicationConfig.java
@@ -2,6 +2,7 @@ package rest;
 
 import java.util.Set;
 import javax.ws.rs.core.Application;
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
 
 
 @javax.ws.rs.ApplicationPath("api")
@@ -10,6 +11,7 @@ public class ApplicationConfig extends Application {
   @Override
   public Set<Class<?>> getClasses() {
     Set<Class<?>> resources = new java.util.HashSet<>();
+    resources.add(MultiPartFeature.class);
     addRestResourceClasses(resources);
     return resources;
   }
@@ -27,11 +29,12 @@ public class ApplicationConfig extends Application {
     resources.add(httpErrors.NotFoundExceptionMapper.class);
     resources.add(rest.Admin.class);
     resources.add(rest.All.class);
+    resources.add(rest.Uploadimage.class);
     resources.add(rest.User.class);
     resources.add(security.JWTAuthenticationFilter.class);
     resources.add(security.Login.class);
     resources.add(security.NotAuthorizedExceptionMapper.class);
-    resources.add(security.RolesAllowedFilter.class);
+        resources.add(security.RolesAllowedFilter.class);
   }
   
 }
